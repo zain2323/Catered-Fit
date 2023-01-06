b74e87256032fe4b7dd77e7ba566aa4e502186d7
Zain Siddiqui <zainsiddiqui2323@gmail.com>
2023-01-06 11:20:33 +0500

    deployed ML model

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, jsonify
+import pickle
+from model import transform_input, getFoodName, initialize
+import numpy as np
+
+app = Flask(__name__)
+ml_model = pickle.load(open('trained-model.pkl','rb'))
+ingredients, courses, X_dict, X, y = initialize()
+
+@app.get("/")
+def index():
+    return render_template("homepage.html")
+
+@app.get('/api')
+def predict():
+    my_ingredients = ["biryani masala", "yoghurt", "sugar", "flour", "green chillies", "rice", "chicken", "milk"]
+    # my_ingredients = ["yoghurt", "sugar", "flour", "rice", "milk", "almonds", "sugar syrup", "dry fruits"]
+    course = ["starter"]
+    encoded_input = transform_input(my_ingredients, course, courses, ingredients)
+    probabilities = ml_model.predict_proba(encoded_input)
+    predictions = np.argsort(-probabilities).flatten()
+    top_5_foods = getFoodName(predictions[0:5], X_dict)
+    return render_template("homepage.html", foods=top_5_foods)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ model.py
@@ -0,0 +1,126 @@
+import pandas as pd
+import numpy as np
+from sklearn import preprocessing
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+
+df = pd.read_csv("./data/updated_foodsnew.csv")
+
+def unique_ingredients(df):
+    new_ingredients = []
+    ingredients = df["ingredients"]
+    for ingredient in ingredients:
+        lst = ingredient.split(",")
+        lst = list(map(str.lower, lst))
+        for i in lst:
+            if i not in new_ingredients and i != " ":
+                new_ingredients.append(i.strip())
+    new_ingredients = sorted(set(new_ingredients))
+    return new_ingredients
+
+def unique_course(df):
+    return df["course"].unique()
+    
+def hot_encoding(df, courses, ingredients):
+    dataframe = df.copy()
+    for course in courses:
+        dataframe[course] = 0
+        dataframe.loc[dataframe.course.str.contains(course), [course]] = 1
+    for i in ingredients:
+        dataframe[i] = 0
+        dataframe.loc[dataframe.ingredients.str.contains(i), [i]] = 1
+    return dataframe
+
+def encode_course(df, course, courses):
+    dataframe = df.copy()
+    for course in courses:
+        dataframe[course] = 0
+        dataframe.loc[dataframe.course.str.contains(course), [course]] = 1
+    return dataframe
+
+def remove_columns(df):
+    columns = ["name", "ingredients", "diet", "flavor_profile", "course"]
+    return df.drop(columns, axis=1)
+
+def get_encoded_label(df):
+    dataframe = df.copy()
+    label = dataframe["name"]
+    le = preprocessing.LabelEncoder()
+    label_encoded = le.fit_transform(label)
+    dataframe["name_encoded"] = label_encoded
+    encoded = dict(zip(le.classes_, range(len(le.classes_))))
+    return dataframe, encoded 
+
+def split_dataset(dataframe):
+  X = dataframe[dataframe.columns[1:]].values
+  y = dataframe[dataframe.columns[0]].values
+  return X, y
+
+def transform_input(input_ing, course, courses, ingredients):
+    vec = []
+    for c in courses:
+        if c in course:
+            vec.append(1)
+        else:
+            vec.append(0)
+    for ingredient in ingredients:
+        if ingredient in input_ing:
+            vec.append(1)
+        else:
+            vec.append(0)
+    vec = np.array(vec).reshape(1,-1)
+    return vec
+
+def getFoodName(foodIdList, X_dict):
+    food = []
+    for id in foodIdList:
+        for key, value in X_dict.items():
+            if value == id:
+                food.append(key)
+    return food
+
+def get_food_from_idx(df, index):
+    return df[df.index == index]["name"].values[0]
+
+def get_food_idx(df, name_list):
+    ids = []
+    for food in name_list:
+        cnt = 0
+        for name in df["name"]:
+            if food == name:
+                ids.append(cnt)
+            cnt += 1 
+    return ids
+
+def initialize():
+    ingredients = unique_ingredients(df)
+    courses = unique_course(df)
+    dataframe, X_dict = get_encoded_label(df)
+    X = df[df.columns[0]].values
+    cv = CountVectorizer()
+    count_matrix = cv.fit_transform(df["ingredients"])
+    cosine_sim = cosine_similarity(count_matrix)
+    dataframe = hot_encoding(dataframe, courses, ingredients)
+    dataframe = remove_columns(dataframe)
+    X, y = split_dataset(dataframe)
+    return (ingredients, courses, X_dict, X, y)
+
+
+def train(X, y):
+    # Fitting the model
+    rfc = RandomForestClassifier(n_jobs=-1, max_features= 'sqrt' ,n_estimators=100, oob_score = False) 
+    rfc.fit(X,y)
+    print("I got trained")
+
+    # Saving the model
+    pickle.dump(rfc, open('trained-model.pkl','wb'))
+
+
+try:
+    # Loading the model
+    model = pickle.load(open('trained-model.pkl','rb'))
+except:
+    ingredients, courses, X_dict, X, y = initialize()
+    train(X, y)
