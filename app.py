330651b7beeea5756b1bef840364f437eeb47ce4
Zain Siddiqui <zainsiddiqui2323@gmail.com>
2023-01-06 23:40:36 +0500

    integrated main model to predict foo

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import pickle
 from model import transform_input, getFoodName, initialize
 import numpy as np
@@ -11,16 +11,20 @@ ingredients, courses, X_dict, X, y = initialize()
 def index():
     return render_template("homepage.html")
 
-@app.get('/api')
+@app.post('/predict')
 def predict():
-    my_ingredients = ["biryani masala", "yoghurt", "sugar", "flour", "green chillies", "rice", "chicken", "milk"]
-    # my_ingredients = ["yoghurt", "sugar", "flour", "rice", "milk", "almonds", "sugar syrup", "dry fruits"]
-    course = ["starter"]
+    data = request.get_json()
+    course = data["course"]
+    my_ingredients = data["ingredients"]
     encoded_input = transform_input(my_ingredients, course, courses, ingredients)
     probabilities = ml_model.predict_proba(encoded_input)
     predictions = np.argsort(-probabilities).flatten()
-    top_5_foods = getFoodName(predictions[0:5], X_dict)
-    return render_template("homepage.html", foods=top_5_foods)
+    top_5_foods = getFoodName(predictions[0:6], X_dict)
+    response = { "foods": top_5_foods }
+    print(response)
+    return jsonify(response)
+    # print(top_5_foods)
+    # return render_template("homepage.html", foods=top_5_foods)
 
 
 @app.get("/ingredients")
